1ad5a6f428a7f586bcde3f3b444b497b8286f6a5
qkrrlgus114 <qkrrlgus114@naver.com>
2024-04-02 16:02:24 +0900

    Fix : 테스트

--- backend/src/main/java/com/park/restapi/domain/coupon/entity/Coupon.java
+++ backend/src/main/java/com/park/restapi/domain/coupon/entity/Coupon.java
@@ -1,10 +1,7 @@
 package com.park.restapi.domain.coupon.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EntityListeners(AuditingEntityListener.class)
+@ToString
 public class Coupon {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
