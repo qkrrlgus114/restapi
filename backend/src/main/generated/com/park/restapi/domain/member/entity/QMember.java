a04b4644fc6fee4321f761abf3a0ca3e5699ebb7
qkrrlgus114 <qkrrlgus114@naver.com>
2024-04-08 12:01:41 +0900

    Feat : 요청기록 검색 추가

---
+++ backend/src/main/generated/com/park/restapi/domain/member/entity/QMember.java
@@ -0,0 +1,54 @@
+package com.park.restapi.domain.member.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = 1271603036L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final DateTimePath<java.time.LocalDateTime> createDate = createDateTime("createDate", java.time.LocalDateTime.class);
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> loginLastDate = createDateTime("loginLastDate", java.time.LocalDateTime.class);
+
+    public final ListPath<MemberRole, QMemberRole> memberRoles = this.<MemberRole, QMemberRole>createList("memberRoles", MemberRole.class, QMemberRole.class, PathInits.DIRECT2);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<SocialType> socialType = createEnum("socialType", SocialType.class);
+
+    public final NumberPath<Integer> token = createNumber("token", Integer.class);
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
