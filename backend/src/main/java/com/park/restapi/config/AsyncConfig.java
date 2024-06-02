3ed34be040f6942150573460e138db3d66cb8c31
qkrrlgus114 <qkrrlgus114@naver.com>
2024-06-02 21:27:25 +0900

    Fix : 비동기 스레드풀 빈이름 제거

--- backend/src/main/java/com/park/restapi/config/AsyncConfig.java
+++ backend/src/main/java/com/park/restapi/config/AsyncConfig.java
@@ -14,7 +14,7 @@ public class AsyncConfig {
     private static final int MAX_POOL_SIZE = 50;
     private static final int QUEUE_CAPACITY = 20;
 
-    @Bean(name = "taskExecutor")
+    @Bean
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(CORE_POOL_SIZE);
