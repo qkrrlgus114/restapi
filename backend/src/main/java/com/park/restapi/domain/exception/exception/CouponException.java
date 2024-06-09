15aeae1a7041d77cb3c5324b4ec845b7523aa1cf
qkrrlgus114 <qkrrlgus114@naver.com>
2024-06-09 17:31:17 +0900

    Feat : 글로벌핸들러 슬랙 api 추가 및 메시지 전송

--- backend/src/main/java/com/park/restapi/domain/exception/exception/CouponException.java
+++ backend/src/main/java/com/park/restapi/domain/exception/exception/CouponException.java
@@ -4,7 +4,7 @@ import com.park.restapi.domain.exception.info.CouponExceptionInfo;
 import lombok.Getter;
 
 @Getter
-public class CouponException extends RuntimeException {
+public class CouponException extends RuntimeException implements CommonException {
     private CouponExceptionInfo exception;
     // 개발자에게 보여지는 로그
     private String log;
@@ -12,5 +12,15 @@ public class CouponException extends RuntimeException {
     public CouponException(CouponExceptionInfo exception, String log) {
         this.exception = exception;
         this.log = log;
+    }
+
+    @Override
+    public String getLog() {
+        return log;
+    }
+
+    @Override
+    public String getExceptionMessage() {
+        return exception.getMessage();
     }
 }
--- backend/src/main/java/com/park/restapi/domain/exception/exception/MemberException.java
+++ backend/src/main/java/com/park/restapi/domain/exception/exception/MemberException.java
@@ -4,7 +4,7 @@ import com.park.restapi.domain.exception.info.MemberExceptionInfo;
 import lombok.Getter;
 
 @Getter
-public class MemberException extends RuntimeException {
+public class MemberException extends RuntimeException implements CommonException {
     private MemberExceptionInfo exception;
     // 개발자에게 보여지는 로그
     private String log;
@@ -12,5 +12,15 @@ public class MemberException extends RuntimeException {
     public MemberException(MemberExceptionInfo exception, String log) {
         this.exception = exception;
         this.log = log;
+    }
+
+    @Override
+    public String getLog() {
+        return log;
+    }
+
+    @Override
+    public String getExceptionMessage() {
+        return exception.getMessage();
     }
 }
